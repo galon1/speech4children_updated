e574b6ec671f021fcaaa62b0b725d67820f69285
galon1 <gil.alon@mongodb.com>
2026-02-11 21:36:29 -0500

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/speech4children_updated/', // must match repo name
+  base: '/', // must match repo name
 })
